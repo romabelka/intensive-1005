4bcd5d1a20de2d890f78ba09fdb0fd8bb2d48804
Roman Iakobchuk <kaboom.ua@gmail.com>
2020-05-10 09:27:34 +0300

    initial setup

---
+++ client/src/app.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Route, NavLink } from "react-router-dom";
+import AuthPage from "./components/routes/auth";
+import AdminPage from "./components/routes/admin";
+
+function App() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="/auth" activeStyle={{ color: "red" }}>
+              auth
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/admin" activeStyle={{ color: "red" }}>
+              admin
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+      <section>
+        <Route path="/auth" component={AuthPage} />
+        <Route path="/admin" component={AdminPage} />
+      </section>
+    </div>
+  );
+}
+
+export default App;
