00e415f564df1e4336ba8d94e603952d137a4692
Roman Iakobchuk <kaboom.ua@gmail.com>
2020-05-11 11:37:21 +0300

    add gql server

---
+++ graphql/mocks/people.js
@@ -0,0 +1,20 @@
+module.exports = [
+    {
+        _id: "123",
+        firstName: "Test",
+        lastName: "User",
+        email: "test@example.com"
+    },
+    {
+        _id: "124",
+        firstName: "Hohoho",
+        lastName: "Example",
+        email: "example@example.com"
+    },
+    {
+        _id: "125",
+        firstName: "Blalala",
+        lastName: "YAYAYA",
+        email: "here@example.com"
+    },
+]
